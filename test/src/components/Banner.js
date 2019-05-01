fd3c4798fae8496398f115e9e306c09b1ff2e3d6
lhb1032352370 <1032352370@qq.com>
2019-05-01 21:57:43 +0800

    Banner

---
+++ test/src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
+
+
+class Banner extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <section>
+      <div>banner</div>
+    </section>
+  }
+}
+
+export default Banner;
--- test/src/index.js
+++ test/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import propTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.css';
+import Banner from './components/Banner.js'
 
 const root = document.querySelector('#root');
+
+ReactDom.render(<Banner></Banner>, root)
 
 
 
