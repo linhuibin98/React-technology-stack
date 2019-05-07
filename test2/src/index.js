d3303c38504027b85eebfb2228bbd47f0671a308
lhb1032352370 <1032352370@qq.com>
2019-05-07 09:38:02 +0800

    route

--- test2/src/index.js
+++ test2/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import {HashRouter, Route} from 'react-router-dom';
-import A from './components/A'
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import A from './components/A';
+import B from './components/B';
 
 const root = document.querySelector('#root');
 
@@ -17,12 +18,33 @@ const root = document.querySelector('#root');
 于框架开发的时候，需要在WEBPACK中配置多入口，每一个入口对应一个页面；
 */
 
+/**
+ * Switch: 只要有一个路径匹配，就不再往下匹配
+ */
+
+ /**
+  * Redirect: 重定向 
+  * 属性： to=[string] | {pathname, search, state}
+  * 
+  */
+
+// Route的属性
+/**
+ * path: 匹配路径
+ * component: 匹配渲染的组件
+ * render: func  , 当访问路径匹配时，会调用该方法，一般用于“权限校验”（渲染组件之前验证是否存在权限， 做一些特殊处理）
+ * strict: 路径匹配严格模式, 请求路径中存在目标匹配路径即匹配
+ * exact: 请求路径必须和目标路径完全匹配才能匹配
+ */
 
 ReactDom.render((
   <HashRouter>
-    <div>
-      <Route path= '/a' component={ A } />
-    </div>
+    <Switch>
+      <Route path= '/' exact component={ A } />
+      <Route path= '/user' render={() => {
+        return '权限不足！！！'
+      }} />
+    </Switch>
   </HashRouter>
 ), root);
 
