b586a640fcfd4cfd02298e2dde9b09859d4549ac
lhb1032352370 <1032352370@qq.com>
2019-05-10 21:25:06 +0800

    applyMiddleware

--- test_crm/src/store/index.js
+++ test_crm/src/store/index.js
@@ -1,6 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
+import reduxLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
+import reduxPromise from 'redux-promise';
 
-let store = createStore(reducer);
+let store = createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise));
 
 export default store;
